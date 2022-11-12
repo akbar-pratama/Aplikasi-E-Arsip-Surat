4548e5eff9a0310c89d2deced34660231a3b9e16
Dian Akbar Pratama <73422616+akbar-pratama@users.noreply.github.com>
2022-11-12 23:54:12 +0700

    Add files via upload

---
+++ E-Arsip/about.php
@@ -0,0 +1,28 @@
+<?php include("sidebar/sidebar.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container col-md-11">
+		<br>
+		<h2>About</h2><br>
+		<div class="row">
+             <div class="col-4">
+                   <img src="assets/poto.jpg" style="width: 300px; height:400px;">    
+                </div>
+                <div class="col-8">
+                   <h3>Aplikasi ini dibuat oleh:</h3>
+                   <h5>Nama: DIAN AKBAR PRATAMA</h5>
+                   <h5>NIM : 2031730103</h5>
+                   <h5>Tanggal: 09 November 2022</h5>
+                </div>
+            </div>
+        </div>
+	</div>
+</body>
+</html>
